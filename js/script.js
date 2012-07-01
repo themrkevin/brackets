8c611a4a7eef7ee693749d4a7ecf4d019e80d0bc
Kevin Mangubat <kevin.mangu@gmail.com>
2012-07-01 00:19:49 -0400

    Create Player function completed: - Create Player - Add Player into array todo: - Clone template and add player to list

--- js/script.js
+++ js/script.js
@@ -2,19 +2,46 @@
 	Author: Kevin Mangubat
 */
 
-var tournyBracket = {}
-var players = []
+var tBrack = {}
+var playerList = []
+var newPlayer,
+	playerName,
+	playerNumber,
+	$playerList = $('#player-list'),
+	$brackets = $('#brackets'),
+	$listTemplate = $playerList.find('.template'),
+	$listItemName = $playerList.find('.list-item-name'),
+	$newItem = $listTemplate.clone().removeClass('template');
 
-//newPlayer Constructor
+//player constructor
 function Player(name) {
 	this.name = name;
 }
 
-//create player function
-tournyBracket.createPlayer = function(playerName) {
+//create players
+tBrack.createPlayer = function(playerName) {
 	
 	//this creates the new player and assigns the name
-	var aPlayer = new Player(playerName);
+	newPlayer = new Player(playerName);
+	console.log('new player has been added: ', newPlayer);
 
-	
+	//inject new player into array
+	playerList[playerList.length] = newPlayer;
+	console.log('updated list: ', playerList);
+
+	var thisPlayer = playerList[(playerList.length - 1)];
+	console.log(thisPlayer);
+
+	//add players to list
+	//$newItem.find('.list-item-name').text(playerName);
+	tBrack.template($newItem,thisPlayer).appendTo(($playerList));
 };
+
+tBrack.template = function(item,source) {
+	item.find('.list-item-name').text(source.name);
+
+	return item;
+}
+
+
+
