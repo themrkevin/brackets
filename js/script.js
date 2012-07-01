2c6a3d3abc59797dcfea4f8f6977a70492c8223f
Kevin Mangubat <kevin.mangu@gmail.com>
2012-07-01 16:25:14 -0400

    Major Changes: localStorage - now storing whole array rather than individual data todo: removePlayer button

--- js/script.js
+++ js/script.js
@@ -7,15 +7,21 @@ var playerList = []
 var newPlayer,
 	playerName,
 	playerNumber,
+	storedPList,
+	parsedPList,
 	$playerList = $('#player-list'),
 	$brackets = $('#brackets'),
 	$listTemplate = $playerList.find('.template'),
-	$listItemName = $playerList.find('.list-item-name');
+	$listItemName = $playerList.find('.list-item-name'),
+	$playerCount = $playerList.find('.count'),
+	$addPlayer = $playerList.find('.add'),
+	$removePlayer = $playerList.find('.remove');
 
 //player constructor
 function Player(name) {
 	this.name = name;
 }
+
 
 //create players
 tBrack.createPlayer = function(playerName) {
@@ -28,42 +34,69 @@ tBrack.createPlayer = function(playerName) {
 	playerList[playerList.length] = newPlayer;
 	console.log('updated list: ', playerList);
 
-	var thisPosition = (playerList.length - 1);
-	var thisPlayer = playerList[thisPosition];
+	//store playerList locally && reload it as a local object
+	localStorage.setItem('playerList', JSON.stringify(playerList));
+	tBrack.loadPList();
 
-	localStorage.setItem('playerList[' + thisPosition + ']', thisPlayer.name);
+	var thisPosition = (parsedPList.length - 1);
+	var thisPlayer = parsedPList[thisPosition];
 
-	tBrack.addToList(thisPosition);
+	tBrack.addToList(thisPlayer);
+	tBrack.playerCount();
+}
+
+tBrack.loadPList = function() {
+	storedPList = localStorage.getItem('playerList');
+	parsedPList = JSON.parse(storedPList); 
 }
 
 tBrack.template = function(item,source) {
 
 	//find && replace template data
-	item.find('.list-item-name').text(localStorage.getItem('playerList[' + source + ']'));
-
+	item.find('.list-item-name').text(source.name);
 	return item;
 }
 
-tBrack.addToList = function(thisPosition) {
+tBrack.addToList = function(thisPlayer) {
 	
 	//display players to list && clone template
 	var $newItem = $listTemplate.clone().removeClass('template');
-	tBrack.template($newItem,thisPosition).appendTo(($playerList));
+	tBrack.template($newItem,thisPlayer).appendTo(($playerList));
 }
 
 tBrack.repopulate = function() {
-	for ( i = 0; i < localStorage.length; i++ ) {
-		playerList[playerList.length] = localStorage.getItem('playerList[' + i + ']');
 
-		tBrack.addToList(i);
+	tBrack.loadPList();
+	//goes through localStorage and repopulates data
+	for ( i = 0; i < parsedPList.length; i++ ) {
+		playerList[playerList.length] = parsedPList[i];
+		tBrack.addToList(parsedPList[i]);
 	}
+	tBrack.playerCount();
 
 }
 
+tBrack.removePlayer = function() {
+	
+	$('.remove').click(function(){
+		console.log('removal ahoy!');
+		return false;
+	});
+	
+}
+
+tBrack.playerCount = function() {
+	$playerCount.text(parsedPList.length);
+}
+
 $(document).ready(function() {
+
 	if(localStorage.length !== 0) {
 		tBrack.repopulate();
 	} else {
 		console.log('localStorage is empty');
 	}
+
+	tBrack.removePlayer();
+
 });
